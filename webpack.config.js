b527e62e20c5f39f87c687b0a9b3f635f0d37921
Oscar Sue <87151266+oscarraulsue@users.noreply.github.com>
2021-08-27 11:02:15 -0500

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   entry: './src/app/index.js',
+  modo:'production',
   output: {
     filename: 'app.bundle.js',
     path: path.resolve(__dirname, 'build'),
   },
+  plugins: [
+    new HTMLWebpackPlugin({template: './src/index.html'})
+  ],
+
 };
